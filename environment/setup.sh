8da012ff94e89ac208c179cf3889afcf31256ec3
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-01-11 14:26:18 -0300

    Comando setup atualizado

--- environment/setup.sh
+++ environment/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker compose exec app cp .env.example .env
+docker-compose exec app cp .env.testing.example .env.testing
 
 docker compose exec app composer install
 docker compose exec app php artisan migrate:fresh --seed
