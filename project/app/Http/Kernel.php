389179f44692b73985ed15178fb57a2599186cf4
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-01-30 21:45:07 -0300

    Edicao de informacoes do usuario adicionada

--- project/app/Http/Kernel.php
+++ project/app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => Shared\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'check-client-address' => Api\Middleware\Client\CheckClientAddressMiddleware::class,
     ];
 }
--- project/routes/api.php
+++ project/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Api\Controllers\Auth\LoginController;
 use App\Http\Api\Controllers\Auth\LogoutController;
 use App\Http\Api\Controllers\Auth\MeController;
 use App\Http\Api\Controllers\Auth\RegisterController;
+use App\Http\Api\Controllers\Auth\UpdatePersonalDataController;
 use App\Http\Api\Controllers\Client\AddressController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +30,8 @@ Route::prefix('auth')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('me', MeController::class);
         Route::delete('logout', LogoutController::class);
+
+        Route::post('personal-data', UpdatePersonalDataController::class);
     });
 });
 
