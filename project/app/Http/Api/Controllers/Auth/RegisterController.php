01bd12a1454d442f7942536425969e6d5b7614e1
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-03-04 21:58:30 -0300

    Documentacao de endpoints adicionada

--- project/app/Http/Api/Controllers/Auth/RegisterController.php
+++ project/app/Http/Api/Controllers/Auth/RegisterController.php
@@ -10,6 +10,78 @@ use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/auth/register",
+     *     operationId="Regiter",
+     *     tags={"Auth"},
+     *     summary="Register User",
+     *     description="Register a new user",
+     *
+     *     @OA\RequestBody(
+     *
+     *          @OA\JsonContent(
+     *              type="object",
+     *
+     *              @OA\Property(
+     *                   type="string",
+     *                   default="New User",
+     *                   description="New user name",
+     *                   property="name"
+     *               ),
+     *              @OA\Property(
+     *                  type="string",
+     *                  default="example@example.com",
+     *                  description="A valid email address",
+     *                  property="email"
+     *              ),
+     *              @OA\Property(
+     *                  type="string",
+     *                  default="123456",
+     *                  description="New user password",
+     *                  property="password"
+     *              ),
+     *              @OA\Property(
+     *                   type="string",
+     *                   default="123456",
+     *                   description="New user password confirmation",
+     *                   property="password_confirmation"
+     *               )
+     *          ),
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successfully registered user",
+     *
+     *          @OA\JsonContent(
+     *
+     *              @OA\Property(property="message", type="string", example="Usuário criado com sucesso."),
+     *              @OA\Property(property="name", type="string", example="New User")
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *
+     *          @OA\JsonContent(
+     *
+     *              @OA\Property(property="message", type="string", example="Unauthorized")
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad request",
+     *
+     *          @OA\JsonContent(
+     *
+     *              @OA\Property(property="message", type="string", example="Bad request")
+     *          )
+     *      ),
+     * )
+     */
     public function __invoke(Request $request): JsonResponse
     {
         $userData = $request->validate([
