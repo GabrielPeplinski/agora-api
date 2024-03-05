405c47f99d2753287c2c08cc629f911df60312a7
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-03-04 22:27:23 -0300

    Endpoints da api documentados com swagger

--- project/app/Http/Api/Controllers/Auth/LogoutController.php
+++ project/app/Http/Api/Controllers/Auth/LogoutController.php
@@ -6,6 +6,39 @@ use Illuminate\Http\JsonResponse;
 
 class LogoutController
 {
+    /**
+     * @OA\Delete(
+     *     path="/api/client/logout",
+     *     operationId="Logout",
+     *     tags={"Auth"},
+     *     summary="Logout]",
+     *     description="Logout current user",
+     *
+     *      @OA\Response(
+     *          response=204,
+     *          description="Successfully logged out user",
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *
+     *          @OA\JsonContent(
+     *
+     *              @OA\Property(property="message", type="string", example="Não foi possível realizar o logout.")
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad request",
+     *
+     *          @OA\JsonContent(
+     *
+     *              @OA\Property(property="message", type="string", example="Bad request")
+     *          )
+     *      ),
+     * )
+     */
     public function __invoke(): JsonResponse
     {
         $currentUser = auth()->user();
