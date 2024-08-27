4f4168812efd06a4d24864ce4489a012c3d32608
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-08-27 15:50:29 -0300

    Ajustes de github actions

--- project/app/Providers/AuthServiceProvider.php
+++ project/app/Providers/AuthServiceProvider.php
@@ -15,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Address::class => AddressPolicy::class
+        Address::class => AddressPolicy::class,
     ];
 
     /**
