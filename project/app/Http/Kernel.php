870bb8f00bf89250ef6d27017b7a5e3339e37cc1
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-08-28 14:34:47 -0300

    Middleware de validacao de perfil de usuario adicionado

--- project/app/Http/Kernel.php
+++ project/app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check-client-address' => Api\Middleware\Client\CheckClientAddressMiddleware::class,
+        'check-user-role' => Shared\Middleware\CheckUserRoleMiddleware::class,
     ];
 }
---
+++ project/app/Http/Shared/Middleware/CheckUserRoleMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Shared\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserRoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        $userRoles = $request->user()->roles;
+
+        foreach ($userRoles as $role) {
+            if (in_array($role->name, $roles, true)) {
+                return $next($request);
+            }
+        }
+
+        return response()->json([
+            'message' => __('custom.forbidden_route_according_to_current_user_role')
+        ], 403);
+    }
+}
