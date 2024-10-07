f82b889ae362c518190ab70f440fc3a70897f531
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-10-07 16:38:57 -0300

    Testes unitarios para exclusao de user solicitation e funcao de apoio

---
+++ project/tests/Unit/Domains/Solicitation/Actions/Solicitation/DeleteSolicitationActionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\Domains\Solicitation\Actions\Solicitation;
+
+use App\Domains\Solicitation\Actions\Solicitation\DeleteSolicitationAction;
+use App\Domains\Solicitation\Models\Solicitation;
+use Tests\Cases\TestCaseUnit;
+
+class DeleteSolicitationActionTest extends TestCaseUnit
+{
+    public function test_should_delete_a_solicitation()
+    {
+        $solicitationMock = $this->partialMock(Solicitation::class, function ($mock) {
+            $mock->shouldReceive('delete')
+                ->once()
+                ->andReturn(true);
+        });
+
+        (new DeleteSolicitationAction())->execute($solicitationMock);
+    }
+}
