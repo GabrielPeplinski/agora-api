3cf7a45e163c043c1feff62797a3f0d907ce5240
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-10-30 00:26:01 -0300

    Adicao de documentacao para endpoint de atualizacao de status

--- project/app/Http/Api/Controllers/Client/Solicitation/UpdateSolicitationStatusController.php
+++ project/app/Http/Api/Controllers/Client/Solicitation/UpdateSolicitationStatusController.php
@@ -14,6 +14,72 @@ use Illuminate\Validation\ValidationException;
 
 class UpdateSolicitationStatusController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/client/my-solicitations/{mySolicitationId}/status",
+     *     operationId="Update a Solicitation Status",
+     *     tags={"My Solicitations"},
+     *     summary="Update a solicitation status",
+     *     description="Update a current user's solicitation status.",
+     *     security={{"sanctum":{}}},
+     *
+     *     @OA\RequestBody(
+     *
+     *         @OA\JsonContent(
+     *             type="object",
+     *
+     *             @OA\Property(
+     *                 type="string",
+     *                 default="in_progress",
+     *                 description="The new status of the solicitation",
+     *                 property="status",
+     *                 enum={"open", "in_progress", "resolved"},
+     *             ),
+     *         ),
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successfully update a solicitation status",
+     *
+     *         @OA\JsonContent(ref="#/components/schemas/ShowSolicitationResponse")
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request",
+     *
+     *         @OA\JsonContent(
+     *
+     *             @OA\Property(property="message", type="string", example="Bad request")
+     *         )
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *
+     *         @OA\JsonContent(
+     *
+     *             @OA\Property(property="message", type="string", example="Unauthorized")
+     *         )
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *
+     *         @OA\JsonContent(ref="#/components/schemas/ForbiddenResponseExample")
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity",
+     *
+     *         @OA\JsonContent(ref="#/components/schemas/UnprocessableEntityResponseExample")
+     *     )
+     * )
+     */
     public function __invoke(UpdateSolicitationStatusRequest $request, Solicitation $mySolicitation)
     {
         $this->authorize('updateStatus', $mySolicitation);
